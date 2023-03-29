8f84e809544760dc092a481014afd74b4d4d0ddf
Gabriel Torres <thomastgtorres21@gmail.com>
2023-03-28 19:56:45 -0500

    add variables and arrays

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+crystal_types = ["Turquoise", "Citrine", "Onyx", "Amethyst"]
+favorite_numbers = [777, 9, 18, 100]
+soda_prices = [1.11, 2.22, 3.33, 4.44]
+im_always_right = [false, false, false, false]
