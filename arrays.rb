9f900bab403f8825f2b18c87c298e90417afd554
Gabriel Torres <thomastgtorres21@gmail.com>
2023-03-29 10:21:24 -0500

    add methods to arrays

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,20 @@ crystal_types = ["Turquoise", "Citrine", "Onyx", "Amethyst"]
 favorite_numbers = [777, 9, 18, 100]
 soda_prices = [1.11, 2.22, 3.33, 4.44]
 im_always_right = [false, false, false, false]
+
+#Push method add two more elements at the end of the array.
+crystal_types.push("Obsidian", "Jasper")
+p crystal_types
+
+#Pop method will remove the element stored at index 2 from the array.
+favorite_numbers.pop 
+p favorite_numbers
+
+#Shift method will remove the first element from the array.
+soda_prices.shift
+p soda_prices
+
+#Unshift method will add two elements to the begining of the array.
+im_always_right.unshift(true, true)
+p im_always_right 
+
